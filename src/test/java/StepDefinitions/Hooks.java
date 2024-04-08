e30b311bcf91fcef224266951b88dbe916893c28
ermankaratash <48660429+ermankaratash@users.noreply.github.com>
2024-04-08 17:36:18 +0200

    Initial commit

---
+++ src/test/java/StepDefinitions/Hooks.java
@@ -0,0 +1,24 @@
+package StepDefinitions;
+
+import Utilities.GWD;
+import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.io.File;
+
+public class Hooks {
+    @After
+    public void after(Scenario scenario){
+        // taking screenshot
+        if(scenario.isFailed()){
+// The following section is only activated if the extent report plugin is enabled.
+//            TakesScreenshot ts = (TakesScreenshot) GWD.getDriver();
+//            byte[] output = ts.getScreenshotAs(OutputType.BYTES);
+//            scenario.attach(output,"image/png","screenshot name");
+        }
+        GWD.quitDriver();
+    }
+}
+}
