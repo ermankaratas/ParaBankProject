79bfd864d06417114f765b817c92b3c2f6227c12
ermankaratash <48660429+ermankaratash@users.noreply.github.com>
2024-04-13 01:14:14 +0200

    file is corrected

--- src/test/java/Utilities/ConfigReader.java
+++ src/test/java/Utilities/ConfigReader.java
@@ -6,26 +6,22 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class ConfigReader {
-    static Properties properties;
+    private static Properties properties;
 
     static {
+        properties = new Properties();
         try {
             String filePath = "configuration.properties";
             FileInputStream fileInputStream = new FileInputStream(filePath);
-            properties = new Properties();
             properties.load(fileInputStream);
         } catch (FileNotFoundException e) {
             System.out.println("File couldn't be found");
         } catch (IOException e) {
-            System.out.println("File couldn't be load from properties file");
+            System.out.println("File couldn't be load from configuration.properties file");
         }
     }
 
     public static String getProperty(String key) {
         return properties.getProperty(key);
-    }
-
-    public static String get(String keyName) {
-        return properties.getProperty(keyName);
     }
 }
